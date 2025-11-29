caa66383ca0ae7a0fcb40954f993c7ee92e67577
siddheshdatkhile25 <114308312+siddheshdatkhile25@users.noreply.github.com>
2025-11-29 23:53:45 +0530

    Navbar added

---
+++ Frontend/src/Components/user/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Navbar.css'
+import shoppingCartIcon from '../icons/shopping-cart.png'
+import wishlistIcon from '../icons/wishlist.png'
+
+function Navbar() {
+  return (
+    <nav className='navbar' data-bs-theme='dark'>
+  <div className='container-fluid'>
+
+    {/* LEFT SECTION */}
+    <div className="nav-left">
+      <h2 className='navbar-brand'>ElectroKart</h2>
+
+      <ul className='nav-menu'>
+        <li>Shop</li>
+        <li>About Us</li>
+      </ul>
+    </div>
+
+    {/* CENTER SECTION */}
+    <div className="nav-center">
+      <div className="searchbar">
+        <input type="text" placeholder="Search..." />
+      </div>
+    </div>
+
+    <div className="nav-right">
+      <ul className="nav-icons">
+        <li><img src={shoppingCartIcon} alt="Cart" /></li>
+        <li><img src={wishlistIcon} alt="Wishlist" /></li>
+      </ul>
+
+      <button className="login-btn">Login</button>
+    </div>
+
+  </div>
+</nav>
+  )
+}
+
+export default Navbar

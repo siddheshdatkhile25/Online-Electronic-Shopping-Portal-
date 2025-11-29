eeda714f3742a0529b3bc3fd4fae26770a94f1fe
siddheshdatkhile25 <114308312+siddheshdatkhile25@users.noreply.github.com>
2025-11-30 00:29:32 +0530

    Outlet issue solved

--- Frontend/src/Pages/user/Home/Home.jsx
+++ Frontend/src/Pages/user/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from '../../../Components/user/Navbar/Navbar'
-import outlet from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 export default function Home() {
     return (
         <div>
             <Navbar/>
 
-            <outlet/>
+            <Outlet/>
             
         </div>
     )

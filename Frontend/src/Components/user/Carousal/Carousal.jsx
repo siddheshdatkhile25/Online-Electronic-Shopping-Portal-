370c50939118b5d8f83190d56282acff66791881
siddheshdatkhile25 <114308312+siddheshdatkhile25@users.noreply.github.com>
2025-12-07 16:02:52 +0530

    dummy-data added

---
+++ Frontend/src/Components/user/Carousal/Carousal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Slider from "react-slick";
+import './Carousal.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const Carousal = ({ title, products }) => {
+
+  return (
+    <div className="container">
+      <div className="productCarousal">
+        <h2 className="product-title">{title}</h2>
+
+        <div className="card-container">
+            {products.map((item, index) => (
+              <div key={index} className="card">
+                <div className="image">
+                  <img src={item.images[0]} alt={item.name} />
+                </div>
+
+                <div className="data">
+                  <div className="productname">{item.name}</div>
+                  <div className="price">₹{item.price}</div>
+                </div>
+
+                <div className="review">
+                  <div className="rating">
+                    ⭐ {item.rating} ({item.ratingCount})
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousal;
--- Frontend/src/Pages/user/LandingPage/LandingPage.jsx
+++ Frontend/src/Pages/user/LandingPage/LandingPage.jsx
@@ -1,15 +1,50 @@
 import React from 'react'
+import './LandingPage.css'
+import imgicons from '../../../data/carouselImages.json'
+import Carousal from '../../../Components/user/Carousal/Carousal'
+import products from '../../../data/ProductData.json'
+
+const { phones } = products
+
 
 function LandingPage() {
+
     return (
         <div className="container">
             <div className="main-body">
                 <div className="heroSection">
                     <h2>
-                        Discover the Latest in Electronics – Shop Smart, Shop Fast!
+                        Discover the Latest in Electronics Shop Smart, Shop Fast!
                     </h2>
+
+                    <p>
+                        Discover the latest in smart technology, From gadgets to home essentials. Top brands at unbeatable prices, Delivered to your doorstep with ease.
+                    </p>
+
+                    <button className='shopNowbtn'>
+                        Shop Now
+                    </button>
+
                 </div>
+
+                <div className="category-carousel=">
+                    <div className="category-bar">
+                        {imgicons.slice(0, 9).map((icon, index) => (
+                            <div key={index} className='category'>
+                                <div className='category-circle'>
+                                    <div className="category-image">
+                                        <img src={icon.src} alt={icon.alt} title={icon.title} />
+                                    </div>
+                                </div>
+                                <div>{icon.title}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <Carousal title="Great Deals on iPhone Series " products={phones} />
             </div>
+
         </div>
     )
 }

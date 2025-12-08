4173c5d2dabbc2a2ede2319a6bffc5a92bbefa0c
Harshali Patil <harshalispatil11@gmail.com>
2025-12-08 19:41:29 +0530

    Resolved Conflict in Package.json and Main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,20 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-<<<<<<< HEAD
-import {BrowserRouter} from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-=======
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
->>>>>>> origin/Siddhesh
+  </React.StrictMode>,
 )

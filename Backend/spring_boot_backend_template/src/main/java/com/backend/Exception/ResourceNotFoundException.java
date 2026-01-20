681bb7129ed1caab5a6574f9b016ff376643bc07
siddheshdatkhile25 <114308312+siddheshdatkhile25@users.noreply.github.com>
2026-01-20 15:53:48 +0530

    backend Folder structure updated

--- Backend/spring_boot_backend_template/src/main/java/com/backend/Exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.backend.Exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-	public ResourceNotFoundException(String mesg) {
-		super(mesg);
-	}
-
-}
--- Backend/spring_boot_backend_template/src/main/java/com/backend/entities/Status.java
+++
@@ -1,6 +0,0 @@
-package com.backend.entities;
-
-public enum Status {
-	PRESENT, ABSENT, LEAVE;
-
-}
